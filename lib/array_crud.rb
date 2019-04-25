712b095b08d1a7389c8a79b85e0c3eac839ab3a6
Muhammad Musah <mmusah1@gmail.com>
2019-04-25 01:10:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  knicks_players = [Durant,]
+  knicks_players = ["Durant","Kyrie","Zion","Mitchell"]
 end
 
 def add_element_to_end_of_array(array, element)
